9d197ad2e9e65892f4338326107d569be58dde56
hanapiko <apikojuma94@gmail.com>
2025-07-02 19:47:57 +0300

    implement login form rendering via js

---
+++ js/ui.js
@@ -0,0 +1,11 @@
+function renderLoginForm(errorMessage = "") {
+    const app = document.getElementById("app");
+    app.innerHTML = `
+    <form>
+        <h2>Login</h2>
+        <input type="text" id="login-username" placeholder="Username or Email" required>
+        <input type="password" id="login-password" placeholder="Password" required>
+        <button type="submit">Login</button>
+        <div class="error">${errorMessage}</div>
+    </form>`
+}
